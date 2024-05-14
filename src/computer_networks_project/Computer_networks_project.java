91538e7915c528391427a58bb5799b0e7215b4b0
zeynep_mutlu <zeynepmutlu20@gmail.com>
2024-05-14 20:53:00 +0300

    connect-host added by Zeynep Mutlu 210212017

---
+++ src/computer_networks_project/Computer_networks_project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package computer_networks_project;
+
+/**
+ *
+ * @author ZEYNEP
+ */
+public class Computer_networks_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
